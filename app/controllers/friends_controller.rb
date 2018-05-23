1d02dbfdbc5930c74a9c7a2043185d40c71d1e82
Phant0mnyc <edmlounge@gmail.com>
2018-05-23 15:10:58 -0400

    Added bcrypt

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,11 +1,19 @@
 class FriendsController < ApplicationController
-	before_action :authenticate_user
+	# before_action :authenticate_user
 	
   def index
-  	id = params[:id].to_i
-  	if id
-  		render json: User.find(id)
-  	end
+    @friends = []
+    User.find(4).friendships.each do |friendship|
+      @friends << friendship.friend 
+    end
+    render json: @friends
+  end
+
+  def show
+    id = params[:id].to_i
+    if id
+      render json: User.find(id)
+    end
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   post 'user_token' => 'user_token#create'
   get 'friends/index'
   get 'friends/destroy'
+  
   resources :friend_requests
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
